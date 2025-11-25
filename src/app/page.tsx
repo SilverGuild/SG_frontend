7dada0bf857e05c9ed56908f7d6555b0752428f9
elysableu <elysabward@gmail.com>
2025-11-25 15:30:55 -0700

    Fix typo/temp commented -> new workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-
-
 export default function Home() {
   return (
     <div className="min-h-screen h-screen max-h-screen overflow-hidden fixed inset-0 w-full mt-0 flex flex-col justify-center align-items">

dfff2a2ec42855907c20be6675726ac9dcf32cb5
elysableu <elysabward@gmail.com>
2025-11-29 16:33:43 -0700

    First jest tests for CharacterRoster and CharacterRosterCard

---
+++ src/__test__/userHome/components/CharacterRoster.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import CharacterRoster from '@/app/userHome/components/CharacterRoster'
+import { mockCharacters } from '@/lib/mock/characterData'
+import '@testing-library/jest-dom'
+
+describe('CharacterRoster', () => {
+    it('renders all character cards', () => {
+        render(<CharacterRoster characters={mockCharacters} />)
+
+        mockCharacters.forEach((character) => {
+            expect(screen.getByText(character.name)).toBeInTheDocument()
+        })
+    })
+
+    it('renders the correct number of character roster cards', () => {
+        render(<CharacterRoster characters={mockCharacters} />)
+
+        const characterCards = mockCharacters.map(character => {
+            screen.getByText(character.name)
+        })
+
+        expect(characterCards).toHaveLength(mockCharacters.length)
+    })
+
+    it('shows empty state when no characters', () => {
+        render(<CharacterRoster characters={[]} />)
+        
+        expect(screen.getAllByText(/No character data available!/i)).toBeInTheDocument()
+    })
+})
---
+++ src/__test__/userHome/components/CharacterRosterCard.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import CharacterRosterCard from '@/app/userHome/components/CharacterRosterCard'
+import { mockCharacters } from '@/lib/mock/characterData'
+import '@testing-library/jest-dom'
+
+
+
+describe('CharacterRosterCard', () => {
+    const mockCharacter = mockCharacters[0]
+    
+    beforeEach(() => {
+        render(<CharacterRosterCard character={mockCharacter} />)
+    })
+
+    it('renders character token', () => {
+        const img = screen.getByAltText(mockCharacter.name)
+        expect(img).toBeInTheDocument()
+    })
+
+    it('renders character name', () => {
+        expect(screen.getByRole('heading', { level: 3 })).toHaveTextContent(mockCharacter.name)
+    })
+
+    it('renders character level', () => {
+        expect(screen.getByText('Level:')).toBeInTheDocument()
+        expect(screen.getByText(mockCharacter.level.toString())).toBeInTheDocument()
+    })
+    
+    it('renders character class', () => {
+        expect(screen.getByText('Class:')).toBeInTheDocument()
+        expect(screen.getByText(mockCharacter.character_class_id)).toBeInTheDocument()
+    })
+    
+    it('renders character race', () => {
+        expect(screen.getByText('Race:')).toBeInTheDocument()
+        expect(screen.getByText(mockCharacter.race_id)).toBeInTheDocument()
+    })
+
+})

2e6e3006b7c7df08e37afe829b9cfe195946961f
elysableu <elysabward@gmail.com>
2025-12-07 15:33:56 -0700

    Add tests for header naviagtion interactions

---
+++ e2e/tests/layout/header.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect as playwrightExpect } from '@playwright/test'
+
+test.describe('Homepage', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/')
+    })
+
+    test('should display logo', async ({ page }) => {
+        await playwrightExpect(page.getByRole('link', { name: /sg_logo/i})).toBeVisible()
+    })
+
+    test('should navigate to the profile page', async ({ page }) => {
+        await page.getByRole('link', { name: /Profile/i }).click()
+
+        await playwrightExpect(page).toHaveURL('/profile') 
+        await playwrightExpect(page.getByRole('heading', { name: /Profile/i })).toBeVisible()
+    })
+
+    test.skip('should navigate to the about page', async ({ page }) => {
+        await page.getByRole('link', { name: /About/i }).click()
+
+        await playwrightExpect(page).toHaveURL('/about') 
+        await playwrightExpect(page.getByRole('heading', { name: /About SG/i })).toBeVisible()
+    })
+
+    test.skip('should navigate to the create character page', async ({ page }) => {
+        await page.getByRole('link', { name: /character/i }).click()
+
+        await playwrightExpect(page).toHaveURL('/character') 
+        await playwrightExpect(page.getByRole('heading', { name: /Character Creator/i })).toBeVisible()
+    })
+
+    test('should navigate back to the homepage using app logo', async ({ page }) => {
+        await page.getByRole('link', { name: /Profile/i }).click()
+        await playwrightExpect(page).toHaveURL('/profile')
+        
+        await page.getByRole('link', {name: /sg_logo/i}).click()
+        await playwrightExpect(page).toHaveURL('/')
+        await playwrightExpect(page.getByRole('heading', { name: /SilverGuild/i })).toBeVisible()
+    })
+})

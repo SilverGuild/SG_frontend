1a5782bab04182f1fd08225ac38c20e85f5feb67
elysableu <elysabward@gmail.com>
2025-12-09 17:19:01 -0700

    Add fetchUserCharacters to api fetch methods

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { SG_API_BASE_URL, SG_API_ENDPOINTS } from './config'
-import { User } from '@/types/user'
-import { JsonApiResponse, extractSingle } from './jsonApiClient'
+import { User, Character } from '@/types'
+import { JsonApiResponse, extractSingle, extractAll } from './jsonApiClient'
 
 export async function apiRequest<T>(endpoint: string, options?: RequestInit): Promise<T> {
     const response = await fetch(`${SG_API_BASE_URL}${endpoint}`, {
@@ -21,4 +21,9 @@ export async function apiRequest<T>(endpoint: string, options?: RequestInit): Pr
 export async function fetchUser(id: number): Promise<User> {
     const json = await apiRequest<JsonApiResponse<User>>(SG_API_ENDPOINTS.userById(id))
     return extractSingle<User>(json)
+}
+
+export async function fetchUserCharacters(id: number): Promise<Character[]> {
+    const json = await apiRequest<JsonApiResponse<Character>>(SG_API_ENDPOINTS.charactersByUserId(id))
+    return extractAll<Character>(json)
 }

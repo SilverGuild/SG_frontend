0a8709d9ac9597b7399f995dfe43df3efa857c79
elysableu <elysabward@gmail.com>
2026-07-14 15:57:19 -0600

    Add barrel file for character sheet sections

---
+++ src/app/(app)/character/components/sections/index.ts
@@ -0,0 +1,17 @@
+export { default as DeathSaves } from './combat/death-saves/DeathSaves'
+export { default as Defenses } from './combat/defenses/Defenses'
+export { default as HitPoints } from './combat/hit-points/HitPoints'
+
+export { default as Abilities } from './core-stats/abilities/Abilities'
+export { default as Languages } from './core-stats/char-languages/Languages'
+export { default as Inspiration } from './core-stats/inspiration/Inspiration'
+export { default as Passives } from './core-stats/passives/Passives'
+export { default as Proficiencies } from './core-stats/proficiencies/Proficiencies'
+export { default as ProficicencyBonus } from './core-stats/proficiencies/ProficiencyBonus'
+export { default as SavingThrows } from './core-stats/saving-throws/SavingThrows'
+export { default as Skills } from './core-stats/skills/Skills'
+
+export { default as Identity } from './identity/Identity'
+
+export { default as Features } from './narrative/features/Features'
+export { default as Traits } from './narrative/traits/Traits'

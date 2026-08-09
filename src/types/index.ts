919d886cfa753b19d3b91efaa8a6e2ba95169abc
elysableu <elysabward@gmail.com>
2026-08-09 13:58:39 -0600

    Update barrel file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,8 @@
-export type { CharacterType, CharacterInput, CharacterMode } from './character'
+export type { CharacterType, CharacterInput, CharacterMode } from './character/character'
+export type { AbilityScoreType, AbilityScoreInput } from './character/abilityScore'
+export type { SkillType, SkillInput } from './character/skill'
+export type { CombatStatsType, CombatStatsInput } from './character/combatStats'
+
 export type { User } from './user'
+
 export type { DataContextType, AppDataSeed } from './dataContext'

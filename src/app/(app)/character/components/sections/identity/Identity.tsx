6913b12a3d753f28bc972ba15cae1ae3eb73561e
elysableu <elysabward@gmail.com>
2026-08-29 13:31:39 -0600

    Update Identity.tsx to use data from CharacterDraftProvider context

--- src/app/(app)/character/components/sections/identity/Identity.tsx
+++ src/app/(app)/character/components/sections/identity/Identity.tsx
@@ -1,24 +1,58 @@
-import { CharacterType } from '@/types'
+'use client'
+
+import { useCharacterDraft } from '@/app/providers'
 import Field from '../../shared/Field'
 
-interface IdentityProps {
-    character?: CharacterType
-    editable?: boolean
-    create?: boolean
-}
 
-export default function Identity({character, editable = false, create = false}: IdentityProps) {
+
+export default function Identity() {
+    const { draft, editable, setField } = useCharacterDraft()
+    
     return (
         <div className="grid grid-cols-2 gap-3 sm:grid-cols-3">
-            {!create ? 
-                <></> : 
-                <Field label="Name" value={character?.name} editable={editable}/>
-            }
-            <Field label="Class" value={character?.character_class_id} editable={editable}/>
-            <Field label="Level" value={character?.level} editable={editable}/>
-            <Field label="Race" value={character?.race_id} editable={editable}/>
-            <Field label="Background" value={character?.background} editable={editable}/>
-            <Field label="Alignment" value={character?.alignment} editable={editable}/>
+            <Field 
+                label="Name" 
+                value={draft.name} 
+                editable={editable}
+                onChange={(value) => setField('name', value)}
+            />
+            <Field 
+                label="Class" 
+                value={draft.character_class_id} 
+                editable={editable}
+                onChange={(value) => setField('character_class_id', value)}
+            />
+            <Field 
+                label="Level" 
+                value={draft.level} 
+                editable={editable}
+                onChange={(value) => {
+                    if (value === '') {
+                        setField('level', 0)
+                        return
+                    }
+                    const parsed = Number(value)
+                    if (!Number.isNaN(parsed)) setField('level', parsed)
+                }}
+            />
+            <Field 
+                label="Race" 
+                value={draft.race_id} 
+                editable={editable}
+                onChange={(value) => setField('race_id', value)}
+            />
+            <Field 
+                label="Background" 
+                value={draft.background} 
+                editable={editable}
+                onChange={(value) => setField('background', value)}
+            />
+            <Field 
+                label="Alignment" 
+                value={draft.alignment} 
+                editable={editable}
+                onChange={(value) => setField('alignment', value)}
+            />
         </div>
     )
 }
